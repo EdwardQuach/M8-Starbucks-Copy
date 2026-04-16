543077b93b297779f230c9476208ac485dd2e94a
EdwardQuach <vkspecialed@gmail.com>
2026-04-16 13:02:41 -0700

    bug fix 3.0

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ elif page == "Exploratory Data Analysis":
         b_selected_col = st.selectbox("Select a numerical column for the box plot:", num_cols)
         if b_selected_col:
             chart_title = f"Distribution of {b_selected_col.title().replace('_', ' ')}"
-            st.plotly_chart(px.box(df, x='Calories', y=b_selected_col, title=chart_title, color='Calories'))
+            st.plotly_chart(px.box(df, x='Calories', y=b_selected_col, title=chart_title, color='Beverage'))
     if 'Scatterplots' in eda_type:
         st.subheader("Scatterplots - Visualizing Relationships")
         num_cols = df.select_dtypes(include=['int64', 'float64']).columns.tolist()
