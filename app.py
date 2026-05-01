852a0fa95667f129c5cd619506bc86ee9f43793f
EdwardQuach <vkspecialed@gmail.com>
2026-05-01 11:37:23 -0700

    Final4

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import subprocess
+import sys
+
+packages = ["streamlit", "pandas", "plotly", "matplotlib", "scikit-learn"]
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", *packages])
+
 import streamlit as st
 import pandas as pd
 import plotly.express as px
